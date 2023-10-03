439ff8f434a746ca2970a7a035c9eb7b71745d6a
Daniel Grimm <danielsgrimm@hotmail.com>
2023-10-02 21:19:41 -0500

    Adding Karaoke site rewritten into Svelte

---
+++ svelte/src/lib/Song.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { videos } from '../songs.ts'
+  const song = videos[Math.floor(Math.random() * 14)]
+</script>
+
+<div>
+  <video width="640" height="480" controls>
+    <source src={song} type="video/mp4">
+    Your browser does not support the video tag.
+  </video>
+  <button>Refresh</button>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  button {
+    max-width: 10rem;
+  }
+</style>
---
+++ svelte/src/songs.ts
@@ -0,0 +1,16 @@
+export const videos: Array<string> = [
+    "https://danielskaraokegame.blob.core.windows.net/videos/ABBA_Dancing-Queen.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Chris-Stapleton_Tennessee-Whiskey.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Estelle_American-Boy.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Florida-Georgia-Line_Meant-To-Be.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Gloria-Gaynor_I-Will-Survive.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/John-Denver_Take-Me-Home-Country-Roads.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Luke-Combs_Beautiful-Crazy.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Marvin-Gaye_Aint-No-Mountain-High-Enough.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Miley-Cyrus_Party-In-The-USA.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Morgan-Wallen_Last-Night.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Neil-Diamond_Sweet-Caroline.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Panic-At-The-Disco_Death-Of-A-Bachelor.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Queen_Bohemian-Rhapsody.mp4",
+    "https://danielskaraokegame.blob.core.windows.net/videos/Taylor-Swift_Love-Story.mp4"
+]
